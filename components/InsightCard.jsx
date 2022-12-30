916a30edb23fdaa8aa0743c5c4ed955658e657d6
gantalaavinash <gantalaavinash@gmail.com>
2022-12-30 10:37:26 +0530

    dirve

--- components/InsightCard.jsx
+++ components/InsightCard.jsx
@@ -6,19 +6,23 @@ import { fadeIn } from '../utils/motion';
 
 const InsightCard = ({ image, title, Description, driveLink, index }) => (
   <motion.div
-    variants={fadeIn('up', 'spring', index * 0.5, 1)}
+    variants={fadeIn('up', 'spring', index * 0.5, 0.5)}
     className="flex md:flex-row flex-col gap-4"
   >
-    <img
-      src={image}
-      alt="planet-01"
-      className="md:w-[270px] w-full h-[250px] rounded-[32px] object-cover"
-    />
+    <a target="_Blank" rel="noreferrer" href={driveLink}>
+      <img
+        src={image}
+        alt="planet-01"
+        className="cursor-pointer md:w-[270px] w-full h-[250px] rounded-[32px] object-cover"
+      />
+    </a>
     <div className="w-full flex justify-between items-center">
       <div className="flex-1 md:ml-[62px] flex flex-col max-w-[650px]">
-        <h4 className="font-mono lg:text-[42px] text-[26px] text-white">
-          {title}
-        </h4>
+        <a target="_Blank" rel="noreferrer" href={driveLink}>
+          <h4 className="cursor-pointer font-mono lg:text-[42px] text-[26px] text-white">
+            {title}
+          </h4>
+        </a>
         <p className="mt-[16px] font-serif lg:text-[20px] text-[14px] text-secondary-white">
           {Description}
         </p>
